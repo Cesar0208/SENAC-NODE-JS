7d77742cc3c331b19e109c86d7408b513f0be61a
César Augusto Soares Lins <Cesar57420926@edu.df.senac.br>
2025-11-10 17:28:35 -0300

    Commit Node JS Aula 1

---
+++ index.js
@@ -0,0 +1,23 @@
+// Criando API Node JS
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+// Criando Rotas
+let users = [
+    {id: 1, nome: 'Cleber'},
+    {id: 2, nome: 'Bianca'},
+    {id: 3, nome: 'Jão'}
+];
+
+// Rota GET
+app.get("/usuarios", (req, res) => {
+    res.json(users);
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+});
